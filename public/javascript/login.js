a0291e146106258dddf92a0e282ea2b1acbbac22
Aaron Lisker <aelisker@gmail.com>
2020-06-10 19:32:38 -0700

    initial commit, but have been working on and restructuring

---
+++ public/javascript/login.js
@@ -0,0 +1,59 @@
+// import { response } from "express";
+
+async function singupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  console.log(`Signing up with username: ${username} and email: ${email}`);
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    // check response status
+    if (response.ok) {
+      console.log('success');
+      // document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', singupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
