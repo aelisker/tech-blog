f6ba7fe0b944d21b33d12377c1a4bb079ee3e9e7
Aaron Lisker <aelisker@gmail.com>
2020-06-14 11:58:27 -0700

    added comment to see if heroku push will work

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const sess = {
   })
 };
 
+// tells app what to use
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
